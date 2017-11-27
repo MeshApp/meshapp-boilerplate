a98c085b28512f8bfd4b43c0f647219a33363e41
Ricardo Pimentel <ricardojrgpimentel@gmail.com>
2017-11-27 14:16:42 +0000

    initial commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/MyComponent.sass'
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+            <div className="my-component">
+                MeshApp Starter Component
+            </div>
+        )
+    }
+};
+
+export default MyComponent
