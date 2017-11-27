8de0a23e293a576fe0848065aec4ae681ba96dcf
Ricardo Pimentel <ricardojrgpimentel@gmail.com>
2017-11-27 14:51:56 +0000

    added comments

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,14 +1,20 @@
 import React from 'react'
+/*
+ * Components have Sass support
+ */
 import '../styles/MyComponent.sass'
+/*
+ * Example Component
+ */
 class MyComponent extends React.Component {
 
-    render() {
-        return (
-            <div className="my-component">
-                MeshApp Starter Component
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="my-component">
+        MeshApp Starter Component
+      </div>
+    )
+  }
 };
 
 export default MyComponent
--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+/*
+ *
+ * MeshApp Component Boilerplate
+ *
+ * This is the main component to load your developed components
+ * You can use this project to develop your component,
+ * or use your own environment and push the changes into this boilerplate
+ *
+ *
+ * This is the place where you need to load your component
+ * The component structure most live inside the folder:
+ * ./components
+ *
+ */
 import MyComponent from './components/MyComponent';
-
+/*
+ * Export here every component
+ * Components must be standalone
+ */
 export { 
   MyComponent,
 }
